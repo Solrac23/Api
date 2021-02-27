16a01b52783f1cfefcf3b5a7c15e67224fd44455
Carlos Eduardo <eduardosantoos573@gmail.com>
2021-02-26 20:55:31 -0400

    my first survey

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import {Router} from 'express'
+import { SurveysController } from './controllers/SurveyController'
+import {UserController} from './controllers/UserController'
+
+const routes = Router()
+
+const userController = new UserController()
+const surveyController = new SurveysController()
+
+routes.post('/users', userController.create)
+routes.get('/surveys', surveyController.show)
+routes.post('/surveys', surveyController.create)
+
+export {routes}
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import {app} from "./app"
+
+app.listen(3335, () => {
+  console.log('Server is running!')
+})
